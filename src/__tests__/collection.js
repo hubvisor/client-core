8c09f21e8c1f89e4849f72f5cb05b6a5f4ca3135
Jérémie Girault <jeremie.girault@gmail.com>
2018-07-02 14:46:25 +0200

    initialized some tests

--- src/__tests__/collection.js
+++ src/__tests__/collection.js
@@ -1,25 +1,55 @@
 import { _reduce } from '../collection'
 
-const sum = (memo, current) => memo + current
-
 describe('_reduce', () => {
+  const accArgs = (memo, value, key, source) => {
+    memo.push({ value, key, source })
+    return memo
+  }
+  const sum = (memo, current) => memo + current
+  const initialValue = 10
+
   describe('Array', () => {
+    const array = [ 1, 2, 3 ]
+    const empty = []
+
     test('without initial value', () => {
-      expect(_reduce([ 1, 2, 3 ], sum)).toEqual([ 1, 2, 3 ].reduce(sum))
+      expect(() => _reduce(array, sum)).toThrow()
     })
 
-    test('with initial value', () => {
-      expect(_reduce([ 1, 2, 3 ], sum, 10)).toEqual([ 1, 2, 3 ].reduce(sum, 10))
+    test('empty', () => {
+      expect(_reduce(empty, sum, initialValue)).toEqual(initialValue)
     })
 
-    test('empty without initial value', () => {
-      expect(() => {
-        _reduce([], sum)
-      }).toThrow()
+    test('non-empty', () => {
+      expect(_reduce(array, sum, initialValue)).toEqual(array.reduce(sum, initialValue))
+    })
+
+    test('parameters', () => {
+      expect(_reduce(array, accArgs, [])).toEqual(array.reduce(accArgs, []))
     })
   })
 
   describe('Object', () => {
+    const object = { foo: 42, bar: 10 }
+    const empty = {}
 
+    test('without initial value', () => {
+      expect(() => _reduce(object, sum)).toThrow()
+    })
+
+    test('empty', () => {
+      expect(_reduce(empty, sum, initialValue)).toEqual(initialValue)
+    })
+
+    test('non-empty', () => {
+      expect(_reduce(object, sum, initialValue)).toEqual(object.foo + object.bar + initialValue)
+    })
+
+    test('parameters', () => {
+      expect(_reduce(object, accArgs, [])).toEqual([ // warning, depends on iteration order on object. Fix me
+        { key: 'foo', value: 42, source: object },
+        { key: 'bar', value: 10, source: object }
+      ])
+    })
   })
 })
--- src/collection.js
+++ src/collection.js
@@ -2,15 +2,16 @@ import { isarray } from './types'
 
 /**
  * A reduce function that can be applied to both arrays or objects.
- * Follows `Array.reduce` for semantics. @see {@link https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/reduce}
+ * Follows `Array.reduce` for semantics excepted that initialValue is required! @see {@link https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/reduce}
  * @param {array|object} source The value to be reduced.
- * @param {function} reducer A callback to be called for each (value, key) pair of the input value.
- * @param {*} initialValue The inital value provided as memo.
+ * @param {function} reducer A callback to be called for each (value, key) pair of the input value with (last, value, key, source) arguments.
+ * @param {*} initialValue The inital value provided as initial "last" value.
  */
 export function _reduce (source, reducer, initialValue) {
-  if (isarray(source)) {
-    // strange bug, passing undefined is not the same as having no second argument
-    return initialValue === undefined ? source.reduce(reducer) : source.reduce(reducer, initialValue)
-  }
-  return Object.entries(source).reduce((memo, [ key, value ]) => reducer(memo, value, key, source), initialValue)
+  if (arguments.length < 3) { throw new Error('initialValue is required for _reduce !') }
+
+  const reduceArray = isarray(source)
+  const collection = reduceArray ? source : Object.entries(source)
+  const iterator = reduceArray ? reducer : (last, [ key, value ]) => reducer(last, value, key, source)
+  return collection.reduce(iterator, initialValue)
 }
