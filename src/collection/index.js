6322a24d48edb8e62e3305eaea8b48e2efcff72e
Jérémie Girault <jeremie.girault@gmail.com>
2018-07-04 18:39:11 +0200

    moved once / added collection index

---
+++ src/collection/index.js
@@ -0,0 +1,19 @@
+import compact from './compact'
+import filter from './filter'
+import flatten from './flatten'
+import groupBy from './groupBy'
+import keyBy from './keyBy'
+import map from './map'
+import mapValues from './mapValues'
+import reduce from './reduce'
+
+export {
+  compact,
+  filter,
+  flatten,
+  groupBy,
+  keyBy,
+  map,
+  mapValues,
+  reduce
+}
